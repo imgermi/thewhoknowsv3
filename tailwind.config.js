da837759e8ce2c084b949c40983568a6c45da725
imgermi <juanmartingermano@gmail.com>
2020-05-23 21:05:52 -0300

    Header layout

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    // screens: {
+    //   sm: '640px',
+    //   md: '768px',
+    //   lg: '1024px',
+    //   xl: '1280px',
+    // },
+    fontFamily: {
+      'sans': ['Ubuntu', 'sans-serif'],
+      'serif': ['Lora', 'serif']
+    },
+    extend: {
+      colors: {
+        primary: '#F43072',
+        secondary: '#494261',
+        black: '#211A39',
+        wkWhite: '#F7F4FD',
+        wkGray: '#EDE8EF'
+      },
+    }
+  }
+}
