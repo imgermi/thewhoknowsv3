bb1c91b52ab62ede571a25078c186e19fece5d31
imgermi <juanmartingermano@gmail.com>
2020-05-25 12:47:53 -0300

    ajustes menu

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   // Hamburger onClick
   $( '#hamburger' ).click(function() {
     $('.menu').toggleClass( 'active' );
+    $('#header').toggleClass( 'active' );
   });
 
 });
