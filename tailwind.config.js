9af8e0034b3b5878b976be4248f7ce74f6ed7d83
imgermi <juanmartingermano@gmail.com>
2020-05-25 21:37:09 -0300

    blog config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   theme: {
     // screens: {
-    //   sm: '640px',
-    //   md: '768px',
-    //   lg: '1024px',
-    //   xl: '1280px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1024px',
     // },
     fontFamily: {
       'sans': ['Ubuntu', 'sans-serif'],
